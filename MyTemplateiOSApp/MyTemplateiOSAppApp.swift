e3ad00a10145e5a7bc28097d0be548bc0fe3c707
hiroakissh <hiroaki.saito1014@gmail.com>
2024-02-10 12:26:13 +0900

    add project

---
+++ MyTemplateiOSApp/MyTemplateiOSAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyTemplateiOSAppApp.swift
+//  MyTemplateiOSApp
+//
+//  Created by HiroakiSaito on 2024/02/10.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyTemplateiOSAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
