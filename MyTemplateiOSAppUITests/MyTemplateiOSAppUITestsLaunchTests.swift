0a10f7efe8684de786fe8c732f5cc15d6519fa43
hiroakissh <hiroaki.saito1014@gmail.com>
2024-02-12 10:41:17 +0900

    [change] コメントアウトはできるだけしたくないのでskip_testingにUItestを追加

--- MyTemplateiOSAppUITests/MyTemplateiOSAppUITestsLaunchTests.swift
+++ MyTemplateiOSAppUITests/MyTemplateiOSAppUITestsLaunchTests.swift
@@ -1,33 +1,32 @@
-//MARK: 使う必要が出てきたらコメントアウトオフ
-////
-////  MyTemplateiOSAppUITestsLaunchTests.swift
-////  MyTemplateiOSAppUITests
-////
-////  Created by HiroakiSaito on 2024/02/10.
-////
 //
-//import XCTest
+//  MyTemplateiOSAppUITestsLaunchTests.swift
+//  MyTemplateiOSAppUITests
 //
-//final class MyTemplateiOSAppUITestsLaunchTests: XCTestCase {
+//  Created by HiroakiSaito on 2024/02/10.
 //
-//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-//        true
-//    }
-//
-//    override func setUpWithError() throws {
-//        continueAfterFailure = false
-//    }
-//
-//    func testLaunch() throws {
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Insert steps here to perform after app launch but before taking a screenshot,
-//        // such as logging into a test account or navigating somewhere in the app
-//
-//        let attachment = XCTAttachment(screenshot: app.screenshot())
-//        attachment.name = "Launch Screen"
-//        attachment.lifetime = .keepAlways
-//        add(attachment)
-//    }
-//}
+
+import XCTest
+
+final class MyTemplateiOSAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
