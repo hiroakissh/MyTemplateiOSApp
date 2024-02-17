c14ea678a665093d77212a72353bf256221a67a8
hiroakissh <hiroaki.saito1014@gmail.com>
2024-02-17 13:17:11 +0900

    [add] add Pacakge

---
+++ App/SampleToDoApp/SampleToDoApp/Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//
+//
+//  Created by HiroakiSaito on 2024/02/17.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "",
+  products: [],
+  dependencies: [],
+  targets: []
+)
---
+++ App/SampleToDoApp/Sources/AppFeature/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//
+//
+//  Created by HiroakiSaito on 2024/02/17.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    // 外部からインスタンス化するためにpublic initializerも追加
+    public init() {}
+
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
