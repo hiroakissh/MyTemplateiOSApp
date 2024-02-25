a59f34b46f4aec33c21e00bfb4ef3fb53a4a7ebe
hiroakissh <hiroaki.saito1014@gmail.com>
2024-02-25 23:02:28 +0900

    [add] TodoActionの追加

---
+++ Presentation/Action/TodoAction.swift
@@ -0,0 +1,16 @@
+//
+//  TodoAction.swift
+//  MyTemplateiOSApp
+//
+//  Created by HiroakiSaito on 2024/02/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+enum TodoAction {
+    case addTodo
+    case updateTodo
+    case readTodo
+    case deleteTodo
+}
