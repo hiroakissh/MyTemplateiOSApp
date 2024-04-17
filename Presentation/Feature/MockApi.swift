46b663a2c2eb0e9342f8cefd73a88320f225e5c9
hiroakissh <hiroaki.saito1014@gmail.com>
2024-04-17 23:06:27 +0900

    [add] ファイルの追加

---
+++ Presentation/Feature/MockApi.swift
@@ -0,0 +1,18 @@
+//
+//  MockApi.swift
+//  MyTemplateiOSApp
+//
+//  Created by HiroakiSaito on 2024/04/17.
+//
+
+import SwiftUI
+
+struct MockApi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MockApi()
+}
