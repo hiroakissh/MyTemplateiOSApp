1ebfe187a20118ab72a64ad80208c2e55d578fd8
hiroakissh <hiroaki.saito1014@gmail.com>
2024-04-21 15:55:03 +0900

    [add] ListViewに基礎情報の追加

--- Presentation/Feature/RepositoryListView.swift
+++ Presentation/Feature/RepositoryListView.swift
@@ -5,4 +5,16 @@
 //  Created by HiroakiSaito on 2024/04/21.
 //
 
+import ComposableArchitecture
+import Domain
 import Foundation
+
+@Reducer
+public struct RepositoryListView {
+    @ObservableState
+    public struct State: Equatable {
+        var repositories: [Domain] = []
+    }
+    public init() {}
+
+}
