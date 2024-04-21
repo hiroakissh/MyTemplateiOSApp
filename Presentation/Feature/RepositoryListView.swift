ea029a0e31ea175d0cd778f9aa7bcff092ff8b25
hiroakissh <hiroaki.saito1014@gmail.com>
2024-04-21 16:10:34 +0900

    [add] 画面表示ににAPIを叩くようにReducer内でリクエスト処理の追加

--- Presentation/Feature/RepositoryListView.swift
+++ Presentation/Feature/RepositoryListView.swift
@@ -22,7 +22,40 @@ public struct RepositoryListView {
     public enum Action {
         case onAppear // 画面が表示された
         case searchRepositoriesResponse(Result<[Repository], Error>) //GitHubAPIRequestの結果を受け取った
-        
+    }
+
+    //  ReducerOf<Self> は Reducer<Self.State, Self.Action>のtypealias型
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                state.isLoading = true
+//                return .none // 外部に対して実行したい処理がないため、Effect.noneを返却する
+                return .run { send in
+                    await send(
+                        .searchRepositoriesResponse(
+                            Result {
+                                let query = "composable"
+                                let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars")!
+                                var request = URLRequest(url: url)
+                                if let token = Bundle.main.infoDictionary?["GithubPersonalAccessToken"] as? String {
+                                    request.setValue(
+                                        "Bearer \(token)",
+                                        forHTTPHeaderField: "Authorization"
+                                    )
+                                }
+                                let (data, _) = try await URLSession.shared.data(for: request)
+                                let repositories = try JSONDecoder().decode(
+                                    GithubSearchResult.self,
+                                    from: data
+                                ).items
+                                return repositories
+                            }
+                        )
+                    )
+                }
+            }
+        }
     }
 
 }
